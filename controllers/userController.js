98909abceedb82f60735725c92118c932357c10a
Sergio Santos <slns@portugalmail.pt>
2020-03-01 18:51:08 +0000

    Add another middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-const authMiddleware = require("../middleware/auth");
+//const authMiddleware = require("../middleware/auth");
+const expressJwt = require('express-jwt');
+//const blacklist = require("express-jwt-blacklist");
+
+const jwtMiddleWare = expressJwt({
+  secret: process.env.TOKEN_KEY
+});
 
 const User = mongoose.model("User");
+
+// router.use(expressJwt({
+//   secret: process.env.TOKEN_KEY,
+//   isRevoked: blacklist.isRevoked
+// }));
 
 router.post("/register", async (req, res) => {
   const { email, username } = req.body;
@@ -43,15 +54,32 @@ router.post("/authenticate", async (req, res) => {
   }
 });
 
-router.use(authMiddleware);
+//router.use(authMiddleware);
 
-router.get("/me", async (req, res) => {
+// router.get("/me", async (req, res) => {
+//   try {
+//     const { userId } = req;
+
+//     const user = await User.findById(userId);
+
+//     return res.json({ user });
+//   } catch (err) {
+//     return res.status(400).json({ error: "Can't get user information" });
+//   }
+// });
+
+// router.get('/logout', function (req, res) {
+//   //console.log('REQ  ',req.headers.authorization)
+//   blacklist.revoke(req.headers.authorization);
+//   res.sendStatus(200);
+// });
+
+router.get("/me", jwtMiddleWare, async (req, res) => {
   try {
-    const { userId } = req;
+    const { user } = req;
+    const userDB = await User.findById(user.id);
 
-    const user = await User.findById(userId);
-
-    return res.json({ user });
+    return res.json({ userDB });
   } catch (err) {
     return res.status(400).json({ error: "Can't get user information" });
   }
